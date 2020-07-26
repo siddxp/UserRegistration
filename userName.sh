9a49a59cdf241b2b6a2317f6b76d77bcfc8819ff
siddxp <siddhantk071@gmail.com>
2020-07-26 21:07:39 +0530

    valid last name added

--- userName.sh
+++ userName.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -x
 read -p "Enter the user first name : " firstName
-pattern="^[A-Z]{1}[a-z]{2}"
-if [[ $firstName =~ $pattern ]]
+pattern1="^[A-Z]{1}[a-z]{2}"
+if [[ $firstName =~ $pattern1 ]]
 then
 	echo "Valid First Name"
 else
 	echo "Invalid First Name"
 fi
 
+
+read -p "Enter the user last name : " lastName
+pattern2="^[A-Z]{1}[a-z]{2}$"
+if [[ $lastName =~ $pattern2 ]]
+then
+   echo "Valid Last Name"
+else
+   echo "Invalid Last Name"
+fi
