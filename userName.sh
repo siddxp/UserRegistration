854a5a2d326a08e7777f8340067223f86caa0b32
siddxp <siddhantk071@gmail.com>
2020-07-26 21:18:24 +0530

    Email validation added

--- userName.sh
+++ userName.sh
@@ -17,3 +17,13 @@ then
 else
    echo "Invalid Last Name"
 fi
+
+
+read -p "Enter the valid e-mail id: " email
+pattern3="^[A-Za-z0-9]{1,}([._+-][0-9a-zA-Z]+)*[@]{1}[a-zA-Z0-9]{1,}\.[A-Za-z]{2,4}([.][a-zA-Z]{2,3}{0,1}$"
+if [[ $email =~ $pattern3 ]]
+then
+	echo "Valid Email id"
+else
+	echo "Invalid Email id"
+fi
